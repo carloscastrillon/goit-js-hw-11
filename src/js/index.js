f516df485cb8b5733edd43be5d332158dffab7a6
Carlos Andres Castrillon <caacastr@bancolombia.com.co>
2023-05-05 21:32:16 -0500

    GHpages

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// Descrito en la documentación
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import GalleryApi from './gallery-fetch';
